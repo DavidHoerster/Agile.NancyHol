93b5375dd5d93591e606f0b0f28d8cf28e1cbe75
David Hoerster <david@agileways.com>
2014-02-20 21:32:52 -0500

    add basic module with two routes

---
+++ NancyRunner/Modules/QuoteModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace NancyRunner.Modules
+{
+    public class QuoteModule : NancyModule
+    {
+        public QuoteModule() : base("/quote")   //base address for routes in this module
+        {
+            Get["/"] = _ =>
+            {
+                return "Hello Nancy!";
+            };
+
+            Get["/{id}"] = args =>
+            {
+                return "Hello Nancy for ID " + args.id;
+            };
+        }
+    }
+}
