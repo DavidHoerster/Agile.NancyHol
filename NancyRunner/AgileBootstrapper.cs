427b928f77fdab39f60278fcb90fbeb8d0713881
David Hoerster <david@agileways.com>
2014-02-20 22:18:31 -0500

    add static JACSHT files via bootstrapper

---
+++ NancyRunner/AgileBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Conventions;
+
+namespace NancyRunner
+{
+    public class AgileBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions conventions)
+        {
+            base.ConfigureConventions(conventions);
+
+            //set this up to define a directory of static assets
+            // contains my JS, CSS, Images, etc.
+            conventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("/assets")
+            );
+        }
+    }
+}
