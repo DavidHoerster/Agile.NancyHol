770843615cb29c42832581de3cee423ea147ae54
David Hoerster <david@agileways.com>
2014-02-20 21:05:58 -0500

    initial project commit

---
+++ NancyRunner/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy.Hosting.Self;
+
+namespace NancyRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting up Minimalist Service");
+
+            Console.WriteLine("Start host configuration");
+            var nancyConfig = new HostConfiguration()
+            {
+                UrlReservations = new UrlReservations()
+                {
+                    CreateAutomatically = true,
+                    User = "Everyone"
+                }
+            };
+            var url = ConfigurationManager.AppSettings["minimal.url"];
+
+            Console.WriteLine("Register host URL of {0}", url);
+            var nancyHost = new NancyHost(nancyConfig,
+                                            new Uri(url));
+
+            Console.WriteLine("Start Minimalist Service");
+            nancyHost.Start();
+
+            Console.WriteLine("Host is running.  Hit <ENTER> to shutdown");
+            Console.ReadLine();
+
+            Console.WriteLine("Service is shutting down");
+            nancyHost.Stop();
+
+            Console.WriteLine("Service is shut down.  Bye!");
+        }
+    }
+}
